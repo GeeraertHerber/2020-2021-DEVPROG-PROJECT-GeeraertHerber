a5bbce118bed5dc26c9cf609b35ac72f4ecf32c5
GeeraertHerber <herber.geeraert@student.howest.be>
2020-10-31 00:39:57 +0100

    Start project

---
+++ TinyProject/TinyProject/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TinyProject.Models;
+using Xamarin.Forms;
+
+namespace TinyProject
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            TestModels();
+        }
+
+        public async void TestModels()
+        {
+
+            List<Character> characters = await BBRepository.GetCharactersAsync();
+            Debug.WriteLine($"Aantal Characters: {characters.Count}");
+        }
+    }
+}
---
+++ TinyProject/TinyProject/Models/Character.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TinyProject.Models
+{
+    public class Character
+    {
+        public int char_id { get; set; }
+        public String name { get; set; }
+        public String birthDay { get; set; }
+        public Array occupation { get; set; }
+        public String img { get; set; }
+        public String status { get; set; }
+        public String nickname { get; set; }
+        public Array appearance { get; set; }
+        public String portrayed { get; set; }
+        public Array category { get; set; }
+        public Array better_call_saul_appearance { get; set; }
+    }
+}
