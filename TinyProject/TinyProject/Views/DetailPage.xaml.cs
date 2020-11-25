c01d6738eee27aec7724a49c034061c00d208842
Herber <herber.geeraert@student.howest.be>
2020-11-25 11:12:33 +0100

    FIX: small fixes in data display such as occupations and appearances

--- TinyProject/TinyProject/Views/DetailPage.xaml.cs
+++ TinyProject/TinyProject/Views/DetailPage.xaml.cs
@@ -13,25 +13,43 @@ namespace TinyProject.Views
     public partial class DetailPage : ContentPage
     {
         public int ItemIndex { get; set; }
-        public DetailPage(int itemIndex = 0)
+        public Character ClickedCharacter { get; set; }
+        public DetailPage(int itemIndex = 0, Character clickedCharacter = null)
         {
             ItemIndex = itemIndex;
+            ClickedCharacter = clickedCharacter;
             InitializeComponent();
-            Console.WriteLine(ItemIndex);
             ShowCharacter();
+            
         }
 
         public async void ShowCharacter()
         {
-            List<Character> character = await BBRepository.GetOneCharacterAsync(ItemIndex);
-            Console.WriteLine(character[0].name);
-            lblName.Text = character[0].name;
-            lblAppearance.Text = character[0].AppearanceToString;
-            imgUrl.Source = character[0].img;
-            lblNickname.Text = character[0].nickname;
-            lblStatus.Text = character[0].status;
-            lblOccupation.Text = character[0].OccupationToString;
+            if (ClickedCharacter == null)
+            {
+                List<Character> characters = await BreakingBadRepository.GetOneCharacterAsync(ItemIndex);
+                MakeCharacter(characters);
+            }
+            else
+            {
+                List<Character> characters = new List<Character>();
+                Console.WriteLine(ClickedCharacter.GetType());
+                Console.WriteLine(characters.Count);
+                characters.Add(ClickedCharacter);
+                MakeCharacter(characters);
+            }
+        }
 
+        public void MakeCharacter(List<Character> characters)
+        {
+            Console.WriteLine(characters[0].name);
+            lblName.Text = characters[0].name;
+            lblAppearance.Text = characters[0].AppearanceToString;
+            imgUrl.Source = characters[0].img;
+            lblNickname.Text = characters[0].nickname;
+            lblStatus.Text = characters[0].status;
+            lblOccupation.Text = characters[0].OccupationToString;
+            lblBirthday.Text = characters[0].birthday;
         }
     }
 }
